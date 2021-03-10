7fcb031e95ff097b3508ea339c9aa63d6e8de796
紅梅陽太 <powdersugar828828@gmail.com>
2021-03-11 01:08:50 +0900

    reCAPTCHAでチェックを行うようにして。

--- app/src/handler.ts
+++ app/src/handler.ts
@@ -1,37 +1,57 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import fetch from 'node-fetch';
 import * as AWS from 'aws-sdk';
 import { mailTemplate } from './mail-template';
-import { EventType } from './type';
+import { EventType, RecaptchaResult } from './type';
 import { checkDomain } from './check-domain';
 import { generateResponseHeader } from './respons-header';
+
+require('dotenv').config();
 
 export const sendMail: Handler = async (
   event: APIGatewayEvent, 
   context: Context, 
   callback: Callback
 ) => {
+  // オリジンを取得
+  const origin = event.headers['Origin'];
+
   // リクエストボディの内容を取得
-  const { name, email, message }: EventType = JSON.parse(event.body);
+  const { name, email, message, token }: EventType = JSON.parse(event.body);
 
-  if (!name) throw new Error('名前が入力されていません');
+  const recaptchaRes = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `secret=${process.env.RECAPTCHA_KEY}&response=${token}`,
+  });
 
-  if (!email) throw new Error('メールが入力されていません');
+  const recaptchaResult: RecaptchaResult = await recaptchaRes.json();
 
-  if (!message) throw new Error('お問い合わせ内容が入力されていません');
+  if (!recaptchaResult.success) {
+    callback(null, generateResponseHeader(403, origin, '不正なリクエストが送信されました'));
+  }
+
+  if (!name) {
+    throw new Error('名前が入力されていません');
+  } else if (!email) {
+    throw new Error('メールが入力されていません');
+  } else if (!message) {
+    throw new Error('お問い合わせ内容が入力されていません');
+  }
 
   const ses = new AWS.SES({
     region: 'ap-northeast-1',
   });
 
-  const origin = event.headers['Origin'];
-  
   // メールを送る処理
   try {
     // 許可されていないオリジンからリクエストが送られたらメールを送信しない
     if (!checkDomain(origin)) {
       callback(
         null, 
-        generateResponseHeader(403, origin, '許可されていないオリジンからリクエストが送られました')
+        generateResponseHeader(403, origin, '許可されていないオリジンからリクエストが送信されました')
       );
     } else {
       await ses.sendEmail({
--- app/src/type.ts
+++ app/src/type.ts
@@ -2,4 +2,13 @@ export interface EventType {
   name: string;
   email: string;
   message: string;
+  token: string;
+}
+
+export interface RecaptchaResult {
+  success: boolean;
+  challenge_ts: string;
+  hostname: string,
+  score: number,
+  action: string;
 }
