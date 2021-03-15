0d3dfddbde5f0a20678cdce650838edd43b94583
紅梅陽太 <powdersugar828828@gmail.com>
2021-03-15 22:02:47 +0900

    メイルのテンプレ変更。

--- app/src/mail-template.ts
+++ app/src/mail-template.ts
@@ -6,10 +6,8 @@ const replaceMessage = message.replace(/\n/g, '<br>');
 const body = `<html>
 <body>
   <p>カルキチブログのお問い合わせフォームからメッセージが送信されました。</p>
-  <p>名前</p>
-  <p>${name}</p>
-  <p>メールアドレス</p>
-  <p>${email}</p>
+  <p>名前: ${name}</p>
+  <p>メールアドレス ${email}</p>
   <p>お問い合わせ内容</p>
   <p>${replaceMessage}</p>
 </body>
